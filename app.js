907e61c7e4622640a9efe202e45dd358b2f19efb
Nitin Chaudhary <nitin.io@outlook.com>
2022-11-26 10:30:10 +0530

    module completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
+const { response } = require("express");
 
 const app = express();
 
@@ -13,8 +15,53 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-	res.send("Thank you for Subscribing");
-	console.log(req.body);
+	const fname = req.body.fname;
+	const lname = req.body.lname;
+	const email = req.body.email;
+
+	const data = {
+		members: [
+			{
+				email_address: email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: fname,
+					LNAME: lname
+				}
+			}
+		]
+	}
+
+const jsonData = JSON.stringify(data);
+
+	const url = "https://us21.api.mailchimp.com/3.0/lists/{list-id}";
+	const options = {
+		method: "POST",
+		auth: 'anyString:apiKey'
+	}
+
+	const request = https.request(url, options, (response)=>{
+		if(response.statusCode == 200){
+			res.sendFile(__dirname + "/success.html");
+		}
+	
+		else{
+			res.sendFile(__dirname + "/failure.html");
+		}
+
+		response.on("data", (data)=>{
+			console.log(JSON.parse(data));
+		})
+
+	});
+	// request.write(jsonData);
+	request.end();
+
+
+});
+
+app.post("/failure", (req,res)=>{
+	res.redirect("/");
 });
 
 app.listen(3000, () => {
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,10 @@
+class Pattern1 {
+    void printTriangle(int n) {
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
